103404542aae405df619a1c87bcdaca1cdddcfe4
Kou Fujiwara <kouwasi@gmail.com>
2019-09-12 23:40:40 +0900

    ..

--- reversi_on_discord/__init__.py
+++ reversi_on_discord/__init__.py
@@ -1,11 +1,29 @@
+import numpy as np
 from discord import Client
 
+from reversi_on_discord.enums import Piece
+
 client = Client()
+inital_board = _initial_board_generator()
+
 
 @client.event
 async def on_ready():
     print('Ready!')
 
+
 @client.event
 async def on_message(message):
-    pass
+    if message.author == client.user:
+        return
+
+
+def _initial_board_generator():
+    board = np.array([[None for i in range(8)] for j in range(8)])
+    for i in range(3, 5):
+        board[i][i] = Piece.WHITE
+
+    board[3][4] = Piece.BLACK
+    board[4][3] = Piece.BLACK
+
+    return board
---
+++ reversi_on_discord/enums.py
@@ -0,0 +1,5 @@
+from enum import Enum
+
+class Piece(Enum):
+    WHITE = '⚪'
+    BLACK = '⚫'
